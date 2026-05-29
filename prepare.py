e2cde42bb59841f02cf5ce4997f942a0bb3ab97a
dderedor <119873849+dderedor@users.noreply.github.com>
2026-05-29 22:19:43 +0500

    Create prepare.py

---
+++ prepare.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import os
+
+df = sns.load_dataset('diamonds')
+# Удаляем строки с нулевыми размерами (выбросы)
+df = df[(df[['x','y','z']] != 0).all(axis=1)]
+df = df.dropna()
+os.makedirs('data', exist_ok=True)
+df.to_csv('data/prepared.csv', index=False)
+print('Подготовленные данные сохранены в data/prepared.csv')
